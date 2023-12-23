14c69c0f2655ac9d880e4ee480b3c8f081c44685
vcielka666 <romanmarko90@gmail.com>
2023-12-23 12:16:01 +0100

    Images added

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 
 
     const questions = [
-        { question: "Solve for x: 3x = 15.", answer: 5, imageUrl: 'images/image1.jpeg' },
-        { question:"What is the square root of 256? ", answer:16, imageUrl: 'images/image2.jpeg'},
-        { question:"How many edges does a cube have? ", answer:12, imageUrl: 'images/image3.jpeg'},
-        { question:"What is 7 multiplied by 2?", answer:14, imageUrl: 'images/image4.jpeg'},
-        { question:"What is the sum of the first three prime numbers?", answer:10, imageUrl: 'images/image5.jpeg'},
-        { question:"If a triangle has angles of 60 and 45 degrees, what is the third angle?", answer:75, imageUrl: 'images/image6.jpeg'},
-        { question:"What is the sum of the angles in a hexagon?", answer:16, imageUrl: 'images/image7.jpeg'},
-        { question:"How many factors does 16 have? ", answer:5, imageUrl: 'images/image8.jpeg'},
-        { question:"Solve for x in the equation: 5x - 7 = 18.", answer:5, imageUrl: 'images/image9.jpeg'},
-        { question:"What is the product of the first two Fibonacci numbers?", answer:1, imageUrl: 'images/image10.jpeg'},
-        { question:"What is the 4th Fibonacci number?", answer:3, imageUrl: 'images/image11.jpeg'},
-        { question:"What is the smallest composite number?", answer:4, imageUrl: 'images/image12.jpeg'},
-        { question:"Solve for x: 3x + 4 = 13", answer:3, imageUrl: 'images/image13.jpeg'},
-        { question:"How many diagonals does a hexagon have? ", answer:9, imageUrl: 'images/image14.jpeg'},
-        { question:"How many faces does a dodecahedron have?", answer:12, imageUrl: 'images/image15.jpeg'},
-        { question:"How many prime numbers are there between 10 and 30?", answer:6, imageUrl: 'images/image16.jpeg'},
-        { question:"If a hexagon's each side is 2 units, what is its perimeter? ", answer:12, imageUrl: 'images/image17.jpeg'},
+        { question: "Solve for x: 3x = 15.", answer: 5, imageUrl: 'images/image0.jpeg' },
+        { question:"What is the square root of 256? ", answer:16, imageUrl: 'images/image1.jpeg'},
+        { question:"How many edges does a cube have? ", answer:12, imageUrl: 'images/image2.jpeg'},
+        { question:"What is 7 multiplied by 2?", answer:14, imageUrl: 'images/image3.jpeg'},
+        { question:"What is the sum of the first three prime numbers?", answer:10, imageUrl: 'images/image4.jpeg'},
+        { question:"If a triangle has angles of 60 and 45 degrees, what is the third angle?", answer:75, imageUrl: 'images/image5.jpeg'},
+        { question:"What is the sum of the angles in a hexagon?", answer:16, imageUrl: 'images/image6.jpeg'},
+        { question:"How many factors does 16 have? ", answer:5, imageUrl: 'images/image7.jpeg'},
+        { question:"Solve for x in the equation: 5x - 7 = 18.", answer:5, imageUrl: 'images/image8.jpeg'},
+        { question:"What is the product of the first two Fibonacci numbers?", answer:1, imageUrl: 'images/image9.jpeg'},
+        { question:"What is the 4th Fibonacci number?", answer:3, imageUrl: 'images/image10.jpeg'},
+        { question:"What is the smallest composite number?", answer:4, imageUrl: 'images/image11.jpeg'},
+        { question:"Solve for x: 3x + 4 = 13", answer:3, imageUrl: 'images/image12.jpeg'},
+        { question:"How many diagonals does a hexagon have? ", answer:9, imageUrl: 'images/image13.jpeg'},
+        { question:"How many faces does a dodecahedron have?", answer:12, imageUrl: 'images/image14.jpeg'},
+        { question:"How many prime numbers are there between 10 and 30?", answer:6, imageUrl: 'images/image15.jpeg'},
+        { question:"If a hexagon's each side is 2 units, what is its perimeter? ", answer:12, imageUrl: 'images/image16.jpeg'},
     ];
     document.addEventListener('DOMContentLoaded', function() {
         const gridContainer = document.getElementById('gridContainer');
@@ -26,6 +26,8 @@
         let currentAnswer = 0;
         let activeCell = null;
         let isDigging = false; // To toggle cursor image
+        let currentQuestion = null; // Global variable to keep track of the current question
+
 
            // Custom cursor images
            const cursorImage = 'url(images/cursor.png), auto';
@@ -56,19 +58,29 @@
                 activeCell.style.backgroundColor = ''; // Reset previous cell color
                 activeCell.textContent = ''; // Clear click count from previous cell
             }
+    
             activeCell = cell;
             activeCell.dataset.clicks = 0;
             activeCell.style.backgroundColor = 'lightblue'; // Highlight new active cell
             activeCell.textContent = ''; // Initialize click count text
-            generateMathQuestion();
+    
+            let newQuestion = generateMathQuestion(); // Get the new question
+            if (newQuestion) {
+                currentQuestion = newQuestion; // Update currentQuestion
+                mathQuestion.textContent = currentQuestion.question;
+                mathQuestion.classList.add('questions');
+                currentAnswer = currentQuestion.answer;
+            }
         }
         handleCellClick();
         toggleCursorImage()
     }
+    
 
     function handleCellClick() {
         activeCell.dataset.clicks++;
         activeCell.textContent =  activeCell.dataset.clicks; // Update click count in the cell
+
     }
     
     function updateClickCounter() {
@@ -84,27 +96,96 @@
         confirmButton.addEventListener('click', checkAnswer);
         clickCounter.appendChild(confirmButton);
     
-        function checkAnswer() {
+
+
+
+
+
+        const largeImageContainer = document.getElementById('largeImageContainer');
+        const largeImage = document.getElementById('largeImage');
+    
+        function showLargeImage(imageUrl) {
+            largeImage.src = imageUrl;
+            largeImageContainer.style.display = 'block';
+        }
+    
+        function hideLargeImage() {
+            largeImageContainer.style.display = 'none';
+        }
+
+        questions.forEach(question => question.solved = false);
+
+        function checkAnswer(event) {
+            const wrongAnswerMessages = [
+                'Nie, riadne to prepocitaj!',
+                'To vobec!',
+                'Kusok vedla kamo!',
+                'Zlaticko, chybycka se vloudila',
+                'HM hm hm, skoro spravne!'
+            ];
+        
             if (activeCell && parseInt(activeCell.dataset.clicks) === currentAnswer) {
-                alert('Correct!');
-                // Find the image URL from the current question
-                const correctImage = questions.find(q => q.answer === currentAnswer).imageUrl;
+                alert('JESS bejby!');
+                
+                // Mark the question as solved
+                currentQuestion.solved = true;
+        
+                // Add to usedQuestionsIndices
+                let solvedQuestionIndex = questions.indexOf(currentQuestion);
+                if (!usedQuestionsIndices.includes(solvedQuestionIndex)) {
+                    usedQuestionsIndices.push(solvedQuestionIndex);
+                }
+        
+                console.log("Question solved, added to usedQuestionsIndices:", solvedQuestionIndex);
+        
+                // Use the imageUrl from the currentQuestion
+                const correctImage = currentQuestion.imageUrl;
+                console.log("Showing image:", correctImage);
                 activeCell.style.backgroundImage = `url(${correctImage})`;
-                activeCell.style.backgroundSize = 'cover'; // Ensure the image covers the cell
-                activeCell.style.backgroundPosition = 'center'; // Center the image in the cell
+                activeCell.style.backgroundSize = 'cover';
+                activeCell.style.backgroundPosition = 'center';
+                showLargeImage(correctImage);
             } else {
-                alert('Nope!Try again!');
+                const randomMessage = wrongAnswerMessages[Math.floor(Math.random() * wrongAnswerMessages.length)];
+                alert(randomMessage);
             }
+        
+            event.stopPropagation();
         }
         
-    
-    
-        function generateMathQuestion() {
-            let questionIndex = Math.floor(Math.random() * questions.length);
-            let selectedQuestion = questions[questionIndex];
-            mathQuestion.textContent = selectedQuestion.question;
-            mathQuestion.classList.add('questions');
-            currentAnswer = selectedQuestion.answer;
+
+        
+        
+           // Event listener to close the large image container if clicked outside
+    document.addEventListener('click', function(event) {
+        if (event.target !== largeImage && event.target !== activeCell) {
+            hideLargeImage();
         }
+    });
+        
+    let usedQuestionsIndices = [];
+
+    function generateMathQuestion() {
+        console.log("Used Questions Indices before generating new question:", usedQuestionsIndices);
+    
+        let availableQuestions = questions.filter(q => !q.solved);
+        console.log("Available Questions after filtering:", availableQuestions.length);
+    
+        if (availableQuestions.length === 0) {
+            alert('All questions have been answered!');
+            return null;
+        }
+    
+        let questionIndex = Math.floor(Math.random() * availableQuestions.length);
+        let selectedQuestion = availableQuestions[questionIndex];
+        console.log("Selected Question Index:", questions.indexOf(selectedQuestion));
+    
+        return selectedQuestion; // Return the selected question
+    }
+    
+    
+    
+    
+    
     });
     
